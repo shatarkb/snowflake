3b6b9234bfb101c42278631f43b41344ce6e9d2c
shatarkb <118706179+shatarkb@users.noreply.github.com>
2022-11-20 23:17:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title ('My parents new healty Diner')
+streamlit.title ('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
