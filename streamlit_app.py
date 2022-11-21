7939957472d32ffeeaff744c7e6d083157bfad3f
shatarkb <118706179+shatarkb@users.noreply.github.com>
2022-11-20 23:13:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit
 streamlit.title ('New application to learn APIs')
 streamlit.header ('Database Application')
+streamlit.text ('Multiple databases can be used to store the data.')
+streamlit.text ('These databases can be SQL Server, Oracle, Teradata etc.')
