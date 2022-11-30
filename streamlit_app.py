2fccdce5a5d38f8b10e891f4c52a6748be7deda0
shatarkb <118706179+shatarkb@users.noreply.github.com>
2022-11-30 18:27:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Banana'])
 
 streamlit.dataframe(my_fruit_list)
+
+
+import snowflake.connector
